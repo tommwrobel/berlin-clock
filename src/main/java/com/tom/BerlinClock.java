26edc6b4650a60d8d589da2f72c4b7eaf98dad69
Tomek <tomm.wrobel@gmail.com>
2020-09-27 14:39:35 +0200

    Added method / code refactoring

--- src/main/java/com/tom/BerlinClock.java
+++ src/main/java/com/tom/BerlinClock.java
@@ -1,5 +1,8 @@
 package com.tom;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class BerlinClock {
 
     private ClockBoard clockBoard;
@@ -8,12 +11,26 @@ public class BerlinClock {
         this.clockBoard = new ClockBoard();
     }
 
+    public String getCurrentTimeInBerlinFormat() {
+        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss");
+        Date date = new Date(System.currentTimeMillis());
+        return getBerlinTime(formatter.format(date));
+    }
+
+    public String getBerlinTime(final String time) {
+        setBerlinTime(time);
+        return clockBoard.toString();
+    }
+
     private boolean validateTimeFormat(String time) {
         String pattern = "(?:[01]\\d|2[0-3]):(?:[0-5]\\d):(?:[0-5]\\d)";
         return time.matches(pattern);
     }
 
     private void setBerlinTime(final String time) {
+        if (!validateTimeFormat(time)) {
+            throw new IllegalArgumentException("Invalid time format! Time format should be hh:mm:ss!");
+        }
         String[] splittedTime = time.split(":");
         int hours = Integer.parseInt(splittedTime[0]);
         int minutes = Integer.parseInt(splittedTime[1]);
@@ -22,31 +39,18 @@ public class BerlinClock {
         if (seconds % 2 == 0) {
             clockBoard.setLight(Light.YELLOW, 0, 0);
         }
-
         for (int i = 0; i < hours / 5; i++) {
             clockBoard.setLight(Light.RED, 1, i);
         }
-
         for (int i = 0; i < hours % 5; i++) {
             clockBoard.setLight(Light.RED, 2, i);
         }
-
         for (int i = 0; i < minutes / 5; i++) {
             Light light = (i + 1) % 3 == 0 ? Light.RED : Light.YELLOW;
             clockBoard.setLight(light, 3, i);
         }
-
         for (int i = 0; i < minutes % 5; i++) {
             clockBoard.setLight(Light.YELLOW, 4, i);
         }
     }
-
-    public String getBerlinTime(final String time) {
-        if (!validateTimeFormat(time)) {
-            throw new IllegalArgumentException("Invalid time format! Time format should be hh:mm:ss!");
-        }
-        setBerlinTime(time);
-        return clockBoard.toString();
-    }
-
 }
---
+++ src/main/java/com/tom/Main.java
@@ -0,0 +1,8 @@
+package com.tom;
+
+public class Main {
+    public static void main(String[] args) {
+        BerlinClock berlinClock = new BerlinClock();
+        System.out.println(berlinClock.getCurrentTimeInBerlinFormat());
+    }
+}
