9b5f3a1603919da86e06b453be16a2bb1bcece61
Tomek <tomm.wrobel@gmail.com>
2020-09-27 15:36:54 +0200

    Added readme

--- src/main/java/com/tom/Main.java
+++ src/main/java/com/tom/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         BerlinClock berlinClock = new BerlinClock();
         System.out.println(berlinClock.getCurrentTimeInBerlinFormat());
+        String aaa = 'asd';
     }
 }
